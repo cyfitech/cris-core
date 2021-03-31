13ae1925cdea97d5598ed1e107b0638ddaa43248
Hao Chen <chenhao@cs.utexas.edu>
2021-03-30 22:50:41 -0500

    Helper function to get the type name

---
+++ includes/defs.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <string>
+
+namespace cris::core {
+
+namespace impl {
+
+std::string GetTypeNameFromPrettyFunction(const std::string &pretty_function);
+
+}
+
+// Get the human-friendly type name
+template<class T>
+std::string GetTypeName() {
+    static const std::string name = impl::GetTypeNameFromPrettyFunction(__PRETTY_FUNCTION__);
+    return name;
+}
+
+}  // namespace cris::core
---
+++ tests/defs_test.cc
@@ -0,0 +1,21 @@
+#include "defs.h"
+
+#include "gtest/gtest.h"
+
+namespace cris::core {
+
+class TestType1 {};
+
+template<class T>
+class TestType2 {};
+
+TEST(DefsTest, TypeNameTest) {
+    EXPECT_EQ(GetTypeName<int>(), "int");
+    EXPECT_EQ(GetTypeName<TestType1>(), "cris::core::TestType1");
+    EXPECT_EQ(GetTypeName<TestType2<int>>(), "cris::core::TestType2<int>");
+
+    using TestType3 = TestType1;
+    EXPECT_EQ(GetTypeName<TestType3>(), "cris::core::TestType1");
+}
+
+}  // namespace cris::core
