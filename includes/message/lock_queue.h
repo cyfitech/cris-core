4380d4ccd9a055cd7a074ad8decec4e25e291cbd
Hao Chen <chenhao@cs.utexas.edu>
2021-03-31 02:26:34 -0500

    Lock queue implementation

---
+++ includes/message/lock_queue.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <mutex>
+
+#include "message/queue.h"
+
+namespace cris::core {
+
+class CRMessageLockQueue : public CRMessageQueue {
+   public:
+    CRMessageLockQueue(size_t capacity, CRNodeBase *node, message_processor_t &&processor)
+        : CRMessageQueue(node, std::move(processor))
+        , mCapacity(capacity) {
+        mBuffer.resize(mCapacity, nullptr);
+    }
+
+    void AddMessage(std::shared_ptr<CRMessageBase> &&message) override;
+
+    CRMessageBasePtr PopMessage(bool only_latest) override;
+
+    size_t Size();
+
+    bool IsEmpty();
+
+    bool IsFull();
+
+   private:
+    const size_t                  mCapacity;
+    size_t                        mSize{0};
+    size_t                        mBegin{0};
+    size_t                        mEnd{0};
+    std::mutex                    mMutex;
+    std::vector<CRMessageBasePtr> mBuffer;
+};
+
+}  // namespace cris::core
---
+++ src/lock_queue.cc
@@ -0,0 +1,64 @@
+#include "message/lock_queue.h"
+
+namespace cris::core {
+
+size_t CRMessageLockQueue::Size() {
+    std::lock_guard<std::mutex> lock(mMutex);
+    return mSize;
+}
+
+bool CRMessageLockQueue::IsEmpty() {
+    std::lock_guard<std::mutex> lock(mMutex);
+    return mSize == 0;
+}
+
+bool CRMessageLockQueue::IsFull() {
+    std::lock_guard<std::mutex> lock(mMutex);
+    return mSize >= mCapacity;
+}
+
+void CRMessageLockQueue::AddMessage(std::shared_ptr<CRMessageBase> &&message) {
+    std::lock_guard<std::mutex> lock(mMutex);
+    size_t                      write_pos = mEnd;
+    ++mEnd;
+    if (mEnd >= mCapacity) {
+        mEnd = 0;
+    }
+    if (mSize < mCapacity) {
+        ++mSize;
+    } else {
+        // TODO: WARNING buffer full
+        mSize  = mCapacity;
+        mBegin = mEnd;
+    }
+
+    mBuffer[write_pos] = std::move(message);
+}
+
+CRMessageBasePtr CRMessageLockQueue::PopMessage(bool only_latest) {
+    CRMessageBasePtr            message;
+    size_t                      read_pos;
+    std::lock_guard<std::mutex> lock(mMutex);
+
+    if (mSize == 0) {
+        return nullptr;
+    }
+
+    if (only_latest) {
+        read_pos = mEnd ? (mEnd - 1) : mCapacity;
+        mSize    = 0;
+        mBegin   = 0;
+        mEnd     = 0;
+    } else {
+        read_pos = mBegin;
+        --mSize;
+        ++mBegin;
+        if (mBegin >= mCapacity) {
+            mBegin = 0;
+        }
+    }
+    message = std::move(mBuffer[read_pos]);
+    return message;
+}
+
+}  // namespace cris::core
