c0bccc5385de81aa3b9fcd1bd057370d145c7a08
Hao Chen <chenhao.yalier@gmail.com>
2021-04-08 21:15:40 -0700

    Write stacktraces to glog

--- src/signal.cc
+++ src/signal.cc
@@ -1,9 +1,17 @@
 #include <glog/logging.h>
 
+#include <string>
+
 namespace cris::core {
+
+static void WriteToGlog(const char *data, int size) {
+    std::string msg(data, size);
+    LOG(ERROR) << msg;
+}
 
 void InstallSignalHandler() {
     google::InstallFailureSignalHandler();
+    google::InstallFailureWriter(WriteToGlog);
 }
 
 }  // namespace cris::core
