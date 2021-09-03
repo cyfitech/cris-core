c2083cc6057f6e354dd50783b78e1d409a427842
zhouxindong <13611050407@139.com>
2021-09-03 09:40:59 +0800

    reorder cinttypes

--- src/cr_signal.cc
+++ src/cr_signal.cc
@@ -1,5 +1,4 @@
 #include <glog/logging.h>
-#include <cinttypes>
 
 extern "C" {
 
@@ -10,6 +9,7 @@ extern "C" {
 #include <signal.h>
 
 #include <atomic>
+#include <cinttypes>
 #include <cstdio>
 #include <cstring>
 #include <string>
