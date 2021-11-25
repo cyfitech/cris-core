25c2aef0db9cfbf9bbba00ee16c366b2ffd2ad62
Hao Chen <chenhao.yalier@gmail.com>
2021-11-25 07:28:49 +0000

    Add CRIS_CODE_DIRS

--- scripts/codebase_utils.sh
+++ scripts/codebase_utils.sh
@@ -6,6 +6,12 @@ for cmd in bazel; do
     which "$cmd" >/dev/null
 done
 
+export CRIS_CODE_DIRS="$(sed 's/^[[:space:]]*//' <<< "
+    includes
+    src
+    tests
+" | grep '[^[:space:]]')"
+
 function bazel_common_all () {
     bazel "$@" '//...'
 }
