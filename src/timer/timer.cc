c68d8186784ef9ab649cc3ab998c4d8afa8d22e7
Hao Chen <chenhao.yalier@gmail.com>
2022-03-03 06:25:59 +0000

    Use Rdtsc instead of std::chrono library to get timestamp

--- src/timer/timer.cc
+++ src/timer/timer.cc
@@ -1,12 +1,27 @@
 #include "cris/core/timer/timer.h"
 
+#include <x86intrin.h>
+
+#include <chrono>
+#include <thread>
+
 namespace cris::core {
 
+static const double kTscToNsecRatio = []() {
+    constexpr auto kCalibrationDuration = std::chrono::seconds(1);
+    const auto     start                = std::chrono::steady_clock::now();
+    const auto     start_tick           = __rdtsc();
+    std::this_thread::sleep_for(kCalibrationDuration);
+    const auto end_tick = __rdtsc();
+    const auto end      = std::chrono::steady_clock::now();
+
+    const auto nsec_duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
+
+    return nsec_duration.count() * 1.0 / (end_tick - start_tick);
+}();
+
 cr_timestamp_nsec_t GetSystemTimestampNsec() {
-    using std::chrono::duration_cast;
-    using std::chrono::nanoseconds;
-    using std::chrono::steady_clock;
-    return static_cast<cr_timestamp_nsec_t>(duration_cast<nanoseconds>(steady_clock::now().time_since_epoch()).count());
+    return static_cast<cr_timestamp_nsec_t>(__rdtsc() * kTscToNsecRatio);
 }
 
 cr_timestamp_nsec_t GetUnixTimestampNsec() {
