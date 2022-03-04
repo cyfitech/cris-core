97ab149982e133cd37062a5bc2a66e06908c5163
zhouxindong <13611050407@139.com>
2022-03-04 03:29:09 -0800

    temprory disable -Wconversion for glog

---
+++ includes/cris_glog.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#if defined(__clang__)
+#define CRIS_COMPILER clang
+#elif defined(__GNUG__)
+#define CRIS_COMPILER GCC
+#elif
+#error Only support gcc or clang compiler!
+#endif
+
+#pragma CRIS_COMPILER diagnostic push
+#pragma CRIS_COMPILER diagnostic ignored "-Wconversion"
+#include <glog/logging.h>
+#pragma CRIS_COMPILER diagnostic pop
