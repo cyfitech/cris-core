38c85a3f61a5d0a024029e1206458978c35e52c3
Hao Chen <chenhao.yalier@gmail.com>
2022-03-07 23:40:03 -0800

    Use Rdtsc instead of std::chrono library to get timestamp (#18)

--- src/timer/timer.cc
+++ src/timer/timer.cc
@@ -1,12 +1,48 @@
 #include "cris/core/timer/timer.h"
 
+#ifdef _MSC_VER
+#include <intrin.h>
+#else
+#if __has_include(<x86intrin.h>)
+#include <x86intrin.h>
+#endif
+#endif
+
+#include <chrono>
+#include <thread>
+
 namespace cris::core {
 
+static const double kTscToNsecRatio = []() {
+    constexpr auto kCalibrationDuration = std::chrono::seconds(1);
+
+    unsigned cpuid = 0;
+
+    const auto start      = std::chrono::steady_clock::now();
+    const auto start_tick = GetTSCTick(cpuid);
+    std::this_thread::sleep_for(kCalibrationDuration);
+    const auto end_tick = GetTSCTick(cpuid);
+    const auto end      = std::chrono::steady_clock::now();
+
+    const auto nsec_duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
+
+    return static_cast<double>(nsec_duration.count()) / static_cast<double>((end_tick - start_tick));
+}();
+
+unsigned long long GetTSCTick(unsigned& aux) {
+    /* TODO(xkszltl) RDTSCP is serialized, making it a more robust choice than RDTSC.
+     * However it is not supported by Linux docker on macOS (via VM internally).
+     * Disable it statically until we have better idea for cheap runtime checks.
+     */
+    if constexpr (false) {
+        return __rdtscp(&aux);
+    }
+    return __rdtsc();
+}
+
 cr_timestamp_nsec_t GetSystemTimestampNsec() {
-    using std::chrono::duration_cast;
-    using std::chrono::nanoseconds;
-    using std::chrono::steady_clock;
-    return static_cast<cr_timestamp_nsec_t>(duration_cast<nanoseconds>(steady_clock::now().time_since_epoch()).count());
+    unsigned cpuid = 0;
+    return static_cast<cr_timestamp_nsec_t>(static_cast<double>(GetTSCTick(cpuid)) * kTscToNsecRatio);
 }
 
 cr_timestamp_nsec_t GetUnixTimestampNsec() {
