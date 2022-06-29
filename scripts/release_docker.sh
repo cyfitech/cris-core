75f688cf5341530caba8614c77f69963d87e4373
Hao Chen <chenhao.yalier@gmail.com>
2022-06-29 20:21:40 +0000

    sync release docker script

--- scripts/release_docker.sh
+++ scripts/release_docker.sh
@@ -6,18 +6,20 @@ for cmd in docker sed; do
     which "$cmd" >/dev/null
 done
 
-src='ghcr.io/cyfitech/cris-build:ci'
-
 sudo_docker="$([ -w '/var/run/docker.sock' ] || echo sudo) docker"
 
-$sudo_docker pull "$src"
-date="$($sudo_docker inspect -f '{{.Created}}' "$src" | sed -n 's/\([0-9][0-9]*\)\-\([01][0-9]\)\-\([0-3][0-9]\)T.*/\1\2\3/p')"
-hash="$($sudo_docker inspect -f '{{.Id}}'      "$src" | cut -d: -f2 | sed 's/^\(............\).*/\1/')"
+for dist in ubuntu2{0,2}04 debian11; do
+    repo_name="cris-build-$dist"
+    src="ghcr.io/cyfitech/$repo_name:ci"
 
-[ "$date" ]
-[ "$hash" ]
+    $sudo_docker pull "$src"
+    date="$($sudo_docker inspect -f '{{.Created}}' "$src" | sed -n 's/\([0-9][0-9]*\)\-\([01][0-9]\)\-\([0-3][0-9]\)T.*/\1\2\3/p')"
+    hash="$($sudo_docker inspect -f '{{.Id}}'      "$src" | cut -d: -f2 | sed 's/^\(............\).*/\1/')"
+    [ "$date" ]
+    [ "$hash" ]
 
-for dst in {"$(cut -d: -f1 <<< "$src:")",'cajunhotpot/cris-build'}":$date"{,"-$hash"}; do
-    $sudo_docker tag "$src" "$dst"
-    $sudo_docker push "$dst"
+    for dst in {"$(cut -d: -f1 <<< "$src:")","cajunhotpot/$repo_name"}":$date"{,"-$hash"}; do
+        $sudo_docker tag "$src" "$dst"
+        $sudo_docker push "$dst"
+    done
 done
