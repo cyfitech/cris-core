564a7cf36188965936e723836f16af38cb7b4ba2
Hao Chen <chenhao.yalier@gmail.com>
2022-07-07 21:03:59 +0000

    fuck

--- src/sched/spin_mutex.cc
+++ src/sched/spin_mutex.cc
@@ -3,25 +3,25 @@
 #include <cstddef>
 #include <thread>
 
-#define _ASM_NOOP asm volatile("nop")
+#define _ASM_NOP asm volatile("nop")
 
-// Cannot use `do { } while (0)` here because there would be huge differences between -O0 and -O3
+// Cannot use `do { } while (0)` here because there would be huge differences between -O0 and -O1.
 #define _2X(statement) \
     {                  \
         statement;     \
         statement;     \
     }
 
-#define _2X_NOOP _2X(_ASM_NOOP)
-#define _4X_NOOP _2X(_2X_NOOP)
-#define _8X_NOOP _2X(_4X_NOOP)
-#define _16X_NOOP _2X(_8X_NOOP)
-#define _32X_NOOP _2X(_16X_NOOP)
+#define _2X_NOP _2X(_ASM_NOP)
+#define _4X_NOP _2X(_2X_NOP)
+#define _8X_NOP _2X(_4X_NOP)
+#define _16X_NOP _2X(_8X_NOP)
+#define _32X_NOP _2X(_16X_NOP)
 
 namespace cris::core {
 
 void HybridSpinMutex::lock() {
-    // Spinning for about 500us@3GHz
+    // Spinning for about 500us@3GHz.
     constexpr std::size_t kSpinningCheckLimit = 500;
     for (std::size_t i = 0; i < kSpinningCheckLimit; ++i) {
         if (try_lock()) {
@@ -35,7 +35,7 @@ void HybridSpinMutex::lock() {
         //
         // ~= 1us@3GHz between atomic variable checks.
         for (size_t j = 0; j < kSelfSpinningBetweenChecks; ++j) {
-            _32X_NOOP
+            _32X_NOP
         }
     }
 
