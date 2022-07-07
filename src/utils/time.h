30a3641c30baa139a3f277a53bad1804f1d5d1d7
Hao Chen <chenhao.yalier@gmail.com>
2022-07-07 20:51:00 +0000

    fuck that

--- src/utils/time.h
+++ src/utils/time.h
@@ -7,7 +7,7 @@ namespace cris::core {
 using cr_timestamp_nsec_t = std::int64_t;
 using cr_duration_nsec_t  = std::int64_t;
 
-// CPU timestamp counter tick.
+// CPU Time Stamp Counter tick.
 unsigned long long GetTSCTick(unsigned& aux);
 
 // Monotonic timestamp.
