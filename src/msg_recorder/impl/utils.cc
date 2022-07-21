808075390b85183646878715d9cdb9fa5863221a
Hao Chen <chenhao.yalier@gmail.com>
2022-07-20 20:53:17 -0700

    update

--- src/msg_recorder/impl/utils.cc
+++ src/msg_recorder/impl/utils.cc
@@ -10,13 +10,7 @@ namespace cris::core::impl {
 std::string GetMessageRecordFileName(const std::string& message_type, const CRMessageBase::channel_subid_t subid) {
     auto       message_record_filename   = message_type;
     const auto message_type_replace_pred = [](char c) {
-        static const std::set<char> allowed_punct = {
-            '_',
-            '<',
-            '>',
-            ':',
-        };
-        return !std::isalnum(c) && allowed_punct.find(c) != allowed_punct.end();
+        return !std::isalnum(c);
     };
     std::replace_if(message_record_filename.begin(), message_record_filename.end(), message_type_replace_pred, '_');
     return message_record_filename + "_subid_" + std::to_string(subid) + ".ldb";
