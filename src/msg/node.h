49f7702024893c80318232b36c28d244ce0847fd
Hao Chen <chenhao.yalier@gmail.com>
2022-07-24 02:57:30 +0000

    Make subscription thread-safe

--- src/msg/node.h
+++ src/msg/node.h
@@ -48,9 +48,6 @@ class CRNode {
 
     bool AddMessageToRunner(const CRMessageBasePtr& message);
 
-    // Subscribing functions are NOT thread-safe, do not call concurrently nor call it when messages are coming,
-    // nor call it after runner is running.
-
     template<CRMessageType message_t, CRMessageCallbackType<message_t> callback_t>
     void Subscribe(const channel_subid_t channel_subid, callback_t&& callback, strand_ptr_t strand);
 
