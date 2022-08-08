b6275d3444a6177a58c7a70a07875e04addd0f0f
Hao Chen <chenhao.yalier@gmail.com>
2022-08-08 16:31:43 -0700

    Sync and Upgrade Infra

--- scripts/bazel_wrapper.sh
+++ scripts/bazel_wrapper.sh
@@ -8,10 +8,24 @@ cd "$(dirname "$0")/.."
 
 ! which ccache >/dev/null 2>&1 || ccache -z
 
+if [ "$#" -lt 1 ]; then
+    bazel help
+    exit 0
+fi
+
+BAZEL_CMD="$1"
+shift
+
 # Need to set PATH instead of CC because Bazel does not like '/'s in the CC value.
 PATH="$([ ! "$CC"  ] || dirname  "$CC" ):$([ ! "$CXX" ] || dirname "$CXX"):$PATH"   \
 CC="$(  [ ! "$CC"  ] || basename "$CC" )"                                           \
 CXX="$( [ ! "$CXX" ] || basename "$CXX")"                                           \
-bazel "$@"
+bazel "$BAZEL_CMD"                                                                  \
+      "$(! which ccache >/dev/null 2>&1 || ccache -sv 2>/dev/null                   \
+         | sed -n 's/^[[:space:]]*cache directory[[:space:]:][[:space:]]*//pi'      \
+         | grep .                                                                   \
+         | head -n1                                                                 \
+         | xargs -I{} printf '%s=%s' '--sandbox_writable_path' "{}" )"              \
+      "$@"
 
 ! which ccache >/dev/null 2>&1 || ccache -s
