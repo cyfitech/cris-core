2e76736ea0a3efb463ec7986117e0aca9388a353
Hao Chen <chenhao.yalier@gmail.com>
2022-09-21 18:19:25 +0000

    Fix path handling

--- src/msg_recorder/tools/ldb_record_convert.cc
+++ src/msg_recorder/tools/ldb_record_convert.cc
@@ -3,14 +3,19 @@
 
 #include <gflags/gflags.h>
 
+#include <filesystem>
+
 DEFINE_string(record_path, "", "Path to the old LevelDB record.");
 
 int main(int argc, char* argv[]) {
     gflags::ParseCommandLineFlags(&argc, &argv, true);
     google::InitGoogleLogging(argv[0]);
 
-    cris::core::RecordFile old_file(FLAGS_record_path);
-    cris::core::RecordFile new_file(FLAGS_record_path + ".new.ldb");
+    auto old_file_path = std::filesystem::weakly_canonical(FLAGS_record_path);
+    auto new_file_path = old_file_path.parent_path() / (old_file_path.filename().string() + ".new.ldb");
+
+    cris::core::RecordFile old_file(old_file_path);
+    cris::core::RecordFile new_file(new_file_path);
 
     for (auto itr = old_file.Iterate(); itr.Valid(); itr.Next()) {
         auto [key, value] = itr.Get();
