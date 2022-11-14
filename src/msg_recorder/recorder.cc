f6cfbbf26a3ec5344df0b93e1e835e2fedfbf9f6
yuzhouguo <yuzhouguo@example.com>
2022-11-14 18:17:42 +0800

    update

--- src/msg_recorder/recorder.cc
+++ src/msg_recorder/recorder.cc
@@ -13,7 +13,19 @@ namespace cris::core {
 MessageRecorder::MessageRecorder(const std::filesystem::path& record_dir_prefix, std::shared_ptr<JobRunner> runner)
     : Base(std::move(runner))
     , record_dir_(record_dir_prefix / RecordDirNameGenerator()) {
+    auto temp_record_dir = record_dir_prefix;
+    temp_record_dir += std::string("_snapshots");
+    record_snapshot_dir_ = temp_record_dir;
+
     std::filesystem::create_directories(record_dir_);
+    std::filesystem::create_directories(record_snapshot_dir_);
+
+    Timer t = Timer();
+    for (const auto& [type, pair] : snapshot_intervals_) {
+        GenerateSnapshot(pair.second);
+        const int& alias = pair.second;
+        t.setInterval([this, alias]() { GenerateSnapshot(alias); }, pair.first);
+    }
 }
 
 MessageRecorder::~MessageRecorder() {
@@ -23,6 +35,43 @@ MessageRecorder::~MessageRecorder() {
         LOG(INFO) << "Record dir " << GetRecordDir() << " is empty, removing...";
         std::filesystem::remove(GetRecordDir());
     }
+
+    if (std::filesystem::is_empty(GetRecordSnapshotDir())) {
+        LOG(INFO) << "Record Snapshot dir " << GetRecordSnapshotDir() << " is empty, removing...";
+        std::filesystem::remove(GetRecordSnapshotDir());
+    }
+}
+
+void MessageRecorder::GenerateSnapshot(const int& keep_max) {
+    data_copied_ = false;
+    std::unique_lock lock(recorder_mtx_);
+    snapshot_cv_.wait(lock, [this] { return dataflow_paused; });
+
+    std::filesystem::directory_entry withdraw;
+    int                              counter = 0;
+    for (auto const& dir_entry : std::filesystem::directory_iterator{record_snapshot_dir_}) {
+        counter++;
+        if (!withdraw.exists() ||
+            (withdraw.last_write_time().time_since_epoch().count() -
+             dir_entry.last_write_time().time_since_epoch().count()) > 0) {
+            withdraw = dir_entry;
+        }
+    }
+
+    // withdraw the earliest snapshot if needed
+    if (counter >= keep_max) {
+        std::filesystem::remove_all(withdraw);
+    }
+
+    // generate the latest snapshot dir
+    auto result_record_dir = record_snapshot_dir_;
+    result_record_dir += std::string("/");
+    result_record_dir += std::string(RecordDirNameGenerator());
+    std::filesystem::create_directories(result_record_dir);
+    std::filesystem::copy(record_dir_, result_record_dir, std::filesystem::copy_options::recursive);
+
+    data_copied_ = true;
+    snapshot_cv_.notify_all();
 }
 
 std::string MessageRecorder::RecordDirNameGenerator() {
@@ -31,6 +80,10 @@ std::string MessageRecorder::RecordDirNameGenerator() {
 
 std::filesystem::path MessageRecorder::GetRecordDir() const {
     return record_dir_;
+}
+
+std::filesystem::path MessageRecorder::GetRecordSnapshotDir() const {
+    return record_snapshot_dir_;
 }
 
 RecordFile* MessageRecorder::CreateFile(
--- src/msg_recorder/recorder.h
+++ src/msg_recorder/recorder.h
@@ -4,15 +4,41 @@
 #include "cris/core/msg/node.h"
 #include "cris/core/msg_recorder/record_file.h"
 
+#include <condition_variable>
 #include <filesystem>
 #include <fstream>
 #include <functional>
 #include <iostream>
 #include <memory>
+#include <mutex>
 #include <string>
+#include <thread>
 #include <utility>
 
 namespace cris::core {
+
+class Timer {
+    bool clear = false;
+
+   public:
+    template<typename Function>
+    void setInterval(Function function, int interval);
+};
+
+void Timer::setInterval(auto function, int interval) {
+    this->clear = false;
+    std::thread t([=]() {
+        while (true) {
+            if (this->clear)
+                return;
+            std::this_thread::sleep_for(std::chrono::hours(interval));
+            if (this->clear)
+                return;
+            function();
+        }
+    });
+    t.detach();
+}
 
 class MessageRecorder : public CRNamedNode<MessageRecorder> {
    public:
@@ -21,7 +47,7 @@ class MessageRecorder : public CRNamedNode<MessageRecorder> {
     explicit MessageRecorder(const std::filesystem::path& record_dir_prefix, std::shared_ptr<JobRunner> runner);
 
     MessageRecorder(const MessageRecorder&) = delete;
-    MessageRecorder(MessageRecorder&&)      = default;
+    // MessageRecorder(MessageRecorder&&)      = default;
     MessageRecorder& operator=(const MessageRecorder&) = delete;
     MessageRecorder& operator=(MessageRecorder&&) = delete;
 
@@ -32,15 +58,31 @@ class MessageRecorder : public CRNamedNode<MessageRecorder> {
 
     std::filesystem::path GetRecordDir() const;
 
+    std::filesystem::path GetRecordSnapshotDir() const;
+
    private:
     using msg_serializer = std::function<std::string(const CRMessageBasePtr&)>;
 
     RecordFile* CreateFile(const std::string& message_type, const channel_subid_t subid, const std::string& alias);
 
+    void GenerateSnapshot(const int& keep_max);
+
     static std::string RecordDirNameGenerator();
 
     const std::filesystem::path              record_dir_;
+    std::filesystem::path                    record_snapshot_dir_;
     std::vector<std::unique_ptr<RecordFile>> files_;
+
+    std::map<std::string, std::pair<int, int>> snapshot_intervals_{
+        {"HOURLY", std::make_pair(1, 48)},
+        {"DAILY", std::make_pair(24, 14)},
+        {"WEEKLY", std::make_pair(168, 7)},
+    };
+
+    std::mutex              recorder_mtx_;
+    std::condition_variable snapshot_cv_;
+    bool                    dataflow_paused{false};
+    bool                    data_copied_{false};
 };
 
 template<CRMessageType message_t>
@@ -49,7 +91,16 @@ void MessageRecorder::RegisterChannel(const MessageRecorder::channel_subid_t sub
 
     this->Subscribe<message_t>(
         subid,
-        [record_file](const std::shared_ptr<message_t>& message) { record_file->Write(MessageToStr(*message)); },
+        [this, record_file, alias](const std::shared_ptr<message_t>& message) {
+            if (!data_copied_) {
+                std::unique_lock lock(recorder_mtx_);
+                dataflow_paused = true;
+                snapshot_cv_.notify_all();
+                snapshot_cv_.wait(lock, [this] { return data_copied_; });
+            }
+            record_file->Write(MessageToStr(*message));
+            dataflow_paused = false;
+        },
         /* allow_concurrency = */ false);
 }
 
