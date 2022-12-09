e00260ef1d5914199db5ee24bd4772192a2eae33
yuzhouguo <yuzhouguo@example.com>
2022-12-09 13:36:57 +0800

    update

--- src/config/recorder_config.cc
+++ src/config/recorder_config.cc
@@ -8,31 +8,28 @@
 #include <cstdlib>
 #include <string>
 #include <string_view>
-#include <type_traits>
 
 using namespace std;
 
 namespace cris::core {
-template<class T>
-static void Fail(const T&, const string& info, simdjson::error_code ec) {
+static void Fail(const char* info, simdjson::error_code ec) {
     RAW_LOG_FATAL(
-        "%s: Failed to parse %s object. %s %s",
+        "%s: Failed to parse cris::core::RecorderConfig object. %s %s",
         __func__,
-        core::GetTypeName<remove_cvref_t<T>>().c_str(),
-        info.c_str(),
+        info,
         simdjson::simdjson_error(ec).what());
 }
 
 void ConfigDataParser(RecorderConfig& config, simdjson::ondemand::value& val) {
     simdjson::ondemand::object obj;
     if (const auto ec = val.get(obj)) {
-        Fail(config, "JSON object required.", ec);
+        Fail("JSON object required.", ec);
     }
 
     string_view record_dir_str;
     if (const auto ec = obj["record_dir"].get(record_dir_str)) {
         if (simdjson::simdjson_error(ec).error() != simdjson::NO_SUCH_FIELD) {
-            Fail(config, "Expect a string for \"record_dir\".", ec);
+            Fail("Expect a string for \"record_dir\".", ec);
         }
     }
     config.record_dir_ = string(record_dir_str.data(), record_dir_str.size());
@@ -42,18 +39,18 @@ void ConfigDataParser(RecorderConfig& config, simdjson::ondemand::value& val) {
         if (simdjson::simdjson_error(ec).error() == simdjson::NO_SUCH_FIELD) {
             return;
         }
-        Fail(config, "Expect a list of objects for \"snapshot_intervals\".", ec);
+        Fail("Expect a list of objects for \"snapshot_intervals\".", ec);
     }
 
     for (auto&& data : array_intervals) {
         string_view interval_name;
         if (const auto ec = data["interval_name"].get(interval_name)) {
-            Fail(config, "\"interval_name\" is required.", ec);
+            Fail("\"interval_name\" is required.", ec);
         }
 
         int64_t interval_sec = 0;
         if (const auto ec = data["interval_sec"].get(interval_sec)) {
-            Fail(config, "\"interval_sec\" is required.", ec);
+            Fail("\"interval_sec\" is required.", ec);
         }
 
         config.snapshot_intervals_.push_back(RecorderConfig::IntervalConfig{
