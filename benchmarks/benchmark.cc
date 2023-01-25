59f0ae3e3fa58835a395a333edc455fb5e61c51f
Hao Chen <chenhao.yalier@gmail.com>
2023-01-25 21:44:36 +0000

    Benchmarks for timing facilities

---
+++ benchmarks/benchmark.cc
@@ -0,0 +1,6 @@
+#include "time_benchmark.hpp"
+#include "timer_benchmark.hpp"
+
+#include <benchmark/benchmark.h>
+
+BENCHMARK_MAIN();
---
+++ benchmarks/time_benchmark.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "cris/core/utils/time.h"
+
+#include <benchmark/benchmark.h>
+
+namespace cris::core {
+
+static void BM_GetTSCTick(benchmark::State& state) {
+    unsigned cpuid = 0;
+    for ([[maybe_unused]] auto s : state) {
+        benchmark::DoNotOptimize(GetTSCTick(cpuid));
+    }
+}
+
+static void BM_GetSystemTimestampNsec(benchmark::State& state) {
+    for ([[maybe_unused]] auto s : state) {
+        benchmark::DoNotOptimize(GetSystemTimestampNsec());
+    }
+}
+
+static void BM_GetUnixTimestampNsec(benchmark::State& state) {
+    for ([[maybe_unused]] auto s : state) {
+        benchmark::DoNotOptimize(GetUnixTimestampNsec());
+    }
+}
+
+BENCHMARK(BM_GetTSCTick);
+BENCHMARK(BM_GetSystemTimestampNsec);
+BENCHMARK(BM_GetUnixTimestampNsec);
+
+}  // namespace cris::core
