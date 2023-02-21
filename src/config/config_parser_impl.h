d601bc3eca3ea73794441946ff997729e8154f3c
yangl <flying1225@126.com>
2023-02-21 19:25:03 +0800

    add

--- src/config/config_parser_impl.h
+++ src/config/config_parser_impl.h
@@ -5,7 +5,10 @@
 
 #include <simdjson.h>
 
+#include <chrono>
 #include <cstdlib>
+#include <string>
+#include <type_traits>
 
 namespace cris::core {
 
@@ -26,4 +29,37 @@ inline data_t ConfigDataTrivialParser(simdjson::ondemand::value& val) {
     return res;
 }
 
+template<class T>
+[[noreturn]] static void Fail(const T&, const std::string& info, simdjson::error_code ec) {
+    RAW_LOG_FATAL(
+        "%s: Failed to parse %s object. %s %s",
+        __func__,
+        core::GetTypeName<std::remove_cvref_t<T>>().c_str(),
+        info.c_str(),
+        simdjson::simdjson_error(ec).what());
+    abort();
+}
+
+#define CRIS_CONFIG_PARSE_IMP(config, obj, name, val, is_expert, is_return, extra_message) \
+    if (const auto ec = obj[name].get(val)) {                                        \
+        if (is_expert) {                                                             \
+            if (simdjson::simdjson_error(ec).error() != simdjson::NO_SUCH_FIELD) {   \
+                Fail(config, extra_message, ec);                                     \
+            }                                                                        \
+        } else {                                                                     \
+            Fail(config, extra_message, ec);                                         \
+        }                                                                            \
+        if (is_return) {                                                             \
+            return;                                                                  \
+        }                                                                            \
+    }
+
+#define CRIS_CONFIG_PARSE(config, obj, name, val, extra_message) \
+    CRIS_CONFIG_PARSE_IMP(config, obj, name, val, false, false, extra_message)
+
+#define CRIS_CONFIG_EXPERT_PARSE_NORETURN(config, obj, name, val, extra_message) \
+    CRIS_CONFIG_PARSE_IMP(config, obj, name, val, true, false, extra_message)
+
+#define CRIS_CONFIG_EXPERT_PARSE_RETURN(config, obj, name, val, extra_message) \
+    CRIS_CONFIG_PARSE_IMP(config, obj, name, val, true, true, extra_message)
 }  // namespace cris::core
--- src/msg_recorder/recorder_config.cc
+++ src/msg_recorder/recorder_config.cc
@@ -1,5 +1,6 @@
 #include "recorder_config.h"
 
+#include "cris/core/config/config_parser_impl.h"
 #include "cris/core/utils/defs.h"
 #include "cris/core/utils/logging.h"
 
@@ -12,54 +13,38 @@
 using namespace std;
 
 namespace cris::core {
-static void Fail(const char* info, simdjson::error_code ec) {
-    RAW_LOG_FATAL(
-        "%s: Failed to parse cris::core::RecorderConfig object. %s %s",
-        __func__,
-        info,
-        simdjson::simdjson_error(ec).what());
-}
-
 void ConfigDataParser(RecorderConfig& config, simdjson::ondemand::value& val) {
     simdjson::ondemand::object obj;
     if (const auto ec = val.get(obj)) {
-        Fail("JSON object required.", ec);
+        Fail(config, "JSON object required.", ec);
     }
 
-    string_view record_dir_str;
-    if (const auto ec = obj["record_dir"].get(record_dir_str)) {
-        if (simdjson::simdjson_error(ec).error() != simdjson::NO_SUCH_FIELD) {
-            Fail("Expect a string for \"record_dir\".", ec);
-        }
-    } else {
-        config.record_dir_ = string(record_dir_str.data(), record_dir_str.size());
-    }
+    string_view record_dir;
+    CRIS_CONFIG_EXPERT_PARSE_NORETURN(config, obj, "record_dir", record_dir, "Expect a string for \"record_dir\".");
+    config.record_dir_ = string(record_dir.data(), record_dir.size());
 
-    simdjson::ondemand::array array_intervals;
-    if (const auto ec = obj["snapshot_intervals"].get(array_intervals)) {
-        if (simdjson::simdjson_error(ec).error() == simdjson::NO_SUCH_FIELD) {
-            return;
-        }
-        Fail("Expect a list of objects for \"snapshot_intervals\".", ec);
-    }
+    simdjson::ondemand::array snapshot_intervals;
+    CRIS_CONFIG_EXPERT_PARSE_RETURN(
+        config,
+        obj,
+        "snapshot_intervals",
+        snapshot_intervals,
+        "Expect a list of objects for \"snapshot_intervals\"");
 
-    for (auto&& data : array_intervals) {
+    for (auto&& data : snapshot_intervals) {
         string_view name;
-        if (const auto ec = data["name"].get(name)) {
-            Fail("\"name\" is required.", ec);
-        }
+        CRIS_CONFIG_PARSE(config, data, "name", name, "\"name\" is required.");
 
         uint64_t period_sec = 0;
-        if (const auto ec = data["period_sec"].get(period_sec)) {
-            Fail("\"period_sec\" is required.", ec);
-        }
+        CRIS_CONFIG_PARSE(config, data, "period_sec", period_sec, "\"period_sec\" is required.");
 
         uint64_t max_num_of_copies = RecorderConfig::IntervalConfig::kDefaultMaxNumOfCopies;
-        if (const auto ec = data["max_num_of_copies"].get(max_num_of_copies)) {
-            if (simdjson::simdjson_error(ec).error() != simdjson::NO_SUCH_FIELD) {
-                Fail("\"max_num_of_copies\" must be an unsigned integer.", ec);
-            }
-        }
+        CRIS_CONFIG_EXPERT_PARSE_NORETURN(
+            config,
+            data,
+            "max_num_of_copies",
+            max_num_of_copies,
+            "\"max_num_of_copies\" must be an unsigned integer.");
 
         config.snapshot_intervals_.push_back(RecorderConfig::IntervalConfig{
             .name_              = string(name.data(), name.size()),
