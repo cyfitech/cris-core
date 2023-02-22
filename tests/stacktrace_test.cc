bbed20202f25719198f32568b9e94a09907a1f18
Hao Chen <chenhao.yalier@gmail.com>
2023-02-21 20:56:52 -0800

    tidy

--- tests/stacktrace_test.cc
+++ tests/stacktrace_test.cc
@@ -21,6 +21,8 @@ TEST(StacktraceTest, Basics) {
     InstallSignalHandler();
 
     // Stacktrace should at least include the function names of 2 stack frames.
+    // EXPECT_DEATH contains `goto`.
+    // NOLINTNEXTLINE(cppcoreguidelines-avoid-goto,hicpp-avoid-goto,-warnings-as-errors)
     EXPECT_DEATH(CrashTestFunc(), "cris::core::CrashTestInnerFunc().*\n.*cris::core::CrashTestFunc()");
 }
 
