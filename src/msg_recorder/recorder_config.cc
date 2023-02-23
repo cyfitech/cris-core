9a72a56e7d8ac8c23a90974631f7f18bec380fc2
yangl <flying1225@126.com>
2023-02-23 22:22:58 +0800

    Do not use json parsing macro for special control flow in msg_recorder. (#164)
    为了减少宏里面需要做的事情，把recorder_config.cc代码改回来了。
    recorder_config.cc里面写的目的是，当json文件里面不包含当前字段时，不报错，不炸掉。用默认值处理。

--- src/msg_recorder/recorder_config.cc
+++ src/msg_recorder/recorder_config.cc
@@ -20,25 +20,35 @@ void ConfigDataParser(RecorderConfig& config, simdjson::ondemand::value& val) {
     }
 
     string_view record_dir;
-    CRIS_CONF_JSON_ENTRY_NORETURN(record_dir, obj, config, "Expect a string for \"record_dir\".");
-    config.record_dir_ = string(record_dir.data(), record_dir.size());
+    if (const auto ec = obj["record_dir"].get(record_dir)) {
+        if (simdjson::simdjson_error(ec).error() != simdjson::NO_SUCH_FIELD) {
+            FailToParseConfig(config, "Expect a string for \"record_dir\".", ec);
+        }
+    } else {
+        config.record_dir_ = string(record_dir.data(), record_dir.size());
+    }
 
     simdjson::ondemand::array snapshot_intervals;
-    CRIS_CONF_JSON_ENTRY_RETURN(snapshot_intervals, obj, config, "Expect a list of objects for \"snapshot_intervals\"");
+    if (const auto ec = obj["snapshot_intervals"].get(snapshot_intervals)) {
+        if (simdjson::simdjson_error(ec).error() == simdjson::NO_SUCH_FIELD) {
+            return;
+        }
+        FailToParseConfig(config, "Expect a list of objects for \"snapshot_intervals\".", ec);
+    }
 
     for (auto&& data : snapshot_intervals) {
         string_view name;
-        CRIS_CONF_JSON_ENTRY(name, data, config);
+        CRIS_CONF_JSON_ENTRY(name, data, "name", config);
 
         uint64_t period_sec = 0;
-        CRIS_CONF_JSON_ENTRY(period_sec, data, config);
+        CRIS_CONF_JSON_ENTRY(period_sec, data, "period_sec", config);
 
         uint64_t max_num_of_copies = RecorderConfig::IntervalConfig::kDefaultMaxNumOfCopies;
-        CRIS_CONF_JSON_ENTRY_NORETURN(
-            max_num_of_copies,
-            data,
-            config,
-            "\"max_num_of_copies\" must be an unsigned integer.");
+        if (const auto ec = data["max_num_of_copies"].get(max_num_of_copies)) {
+            if (simdjson::simdjson_error(ec).error() != simdjson::NO_SUCH_FIELD) {
+                FailToParseConfig(config, "\"max_num_of_copies\" must be an unsigned integer.", ec);
+            }
+        }
 
         config.snapshot_intervals_.push_back(RecorderConfig::IntervalConfig{
             .name_              = string(name.data(), name.size()),
