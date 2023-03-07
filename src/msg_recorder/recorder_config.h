8078f7ecaeae6385155b4f84fe7e2813e9a2ba56
zptan <17807777+zptan@users.noreply.github.com>
2023-03-07 13:39:06 +0800

    Parse record rolling config (#178)
    - https://github.com/cyfitech/cris-core/issues/170

--- src/msg_recorder/recorder_config.h
+++ src/msg_recorder/recorder_config.h
@@ -3,13 +3,22 @@
 #include <simdjson.h>
 
 #include <chrono>
+#include <cstdint>
 #include <filesystem>
+#include <limits>
 #include <string>
 #include <vector>
 
 namespace cris::core {
 
 struct RecorderConfig {
+    enum class Rolling {
+        NONE = 0,
+        DAY  = 1,
+        HOUR = 2,
+        SIZE = 3,
+    };
+
     struct IntervalConfig {
         std::string          name_;
         std::chrono::seconds period_;
@@ -20,6 +29,8 @@ struct RecorderConfig {
 
     std::vector<IntervalConfig> snapshot_intervals_;
     std::filesystem::path       record_dir_;
+    Rolling                     rolling_{Rolling::NONE};
+    std::uint64_t               size_limit_mb_{std::numeric_limits<std::uint64_t>::max()};
 };
 
 void ConfigDataParser(RecorderConfig& config, simdjson::ondemand::value& val);
