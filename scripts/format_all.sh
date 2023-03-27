f877c8a2aab96fa65cf8a4195b21a654684b621f
Hao Chen <chenhao.yalier@gmail.com>
2023-03-27 14:29:52 -0700

    macOS support for realpath. (#211)
    macOS 13 is shipped with its BSD version of realpath, without `-e` and
    `-m`.
    Route to grealpath installed by homebrew instead.

--- scripts/format_all.sh
+++ scripts/format_all.sh
@@ -39,32 +39,32 @@ done
 
 # CRLF -> LF.
 # This should not be used on Windows.
-! which dos2unix >/dev/null 2>&1                                                                        \
-|| ls -1A                                                                                               \
-| grep -v '^\.git$'                                                                                     \
-| grep -v '^\.gitignore$'                                                                               \
-| sed 's/\r//g'                                                                                         \
-| tr '\n' '\0'                                                                                          \
-| $(which parallel >/dev/null 2>&1 && echo "parallel -j$(nproc) -m" || echo 'xargs -rI{}') -0           \
-    find {} -type f                                                                                     \
-| sed 's/\r//g'                                                                                         \
-| tr '\n' '\0'                                                                                          \
-| $(which parallel >/dev/null 2>&1 && echo "parallel -j$(nproc) -m" || echo 'xargs -r'   ) -0           \
-    grep -HI "$(printf '\r$')"                                                                          \
-| grep "$(printf ':.*\r$')"                                                                             \
-| cut -d: -f1                                                                                           \
-| grep -v $(set -e;                                                                                     \
-    which git >/dev/null                                                                                \
-    && git submodule foreach -q pwd                                                                     \
-    | xargs -rI{} realpath --relative-to="$(pwd)" {}                                                    \
-    | sed 's/\/*$/\//'                                                                                  \
-    | sed 's/\([\\\/\.\-]\)/\\\1/g'                                                                     \
-    | sed 's/^/\-e\^/'                                                                                  \
-    | grep .                                                                                            \
-    || echo '^$')                                                                                       \
-| sort -u                                                                                               \
-| sed 's/\r//g'                                                                                         \
-| tr '\n' '\0'                                                                                          \
+! which dos2unix >/dev/null 2>&1                                                                                \
+|| ls -1A                                                                                                       \
+| grep -v '^\.git$'                                                                                             \
+| grep -v '^\.gitignore$'                                                                                       \
+| sed 's/\r//g'                                                                                                 \
+| tr '\n' '\0'                                                                                                  \
+| $(which parallel >/dev/null 2>&1 && echo "parallel -j$(nproc) -m" || echo 'xargs -rI{}') -0                   \
+    find {} -type f                                                                                             \
+| sed 's/\r//g'                                                                                                 \
+| tr '\n' '\0'                                                                                                  \
+| $(which parallel >/dev/null 2>&1 && echo "parallel -j$(nproc) -m" || echo 'xargs -r'   ) -0                   \
+    grep -HI "$(printf '\r$')"                                                                                  \
+| grep "$(printf ':.*\r$')"                                                                                     \
+| cut -d: -f1                                                                                                   \
+| grep -v $(set -e;                                                                                             \
+    which git >/dev/null                                                                                        \
+    && git submodule foreach -q pwd                                                                             \
+    | xargs -rI{} "$(! uname -s | grep '^Darwin$' >/dev/null || printf 'g')realpath" --relative-to="$(pwd)" {}  \
+    | sed 's/\/*$/\//'                                                                                          \
+    | sed 's/\([\\\/\.\-]\)/\\\1/g'                                                                             \
+    | sed 's/^/\-e\^/'                                                                                          \
+    | grep .                                                                                                    \
+    || echo '^$')                                                                                               \
+| sort -u                                                                                                       \
+| sed 's/\r//g'                                                                                                 \
+| tr '\n' '\0'                                                                                                  \
 | $(which parallel >/dev/null 2>&1 && echo "parallel -j$(nproc) -m" || echo 'xargs -r'   ) -0 dos2unix -b -v
 
 # If Python formatter is not installed, install it
