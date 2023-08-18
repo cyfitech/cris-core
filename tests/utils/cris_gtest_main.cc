365666aff258f3d772bbc3f2a35354f1b5c58cdf
zptan <zhuopeng.tan@cycleinvestment.cn>
2023-08-18 07:16:55 +0000

    Allow to pass gflags argument to tests

--- tests/utils/cris_gtest_main.cc
+++ tests/utils/cris_gtest_main.cc
@@ -1,11 +1,14 @@
 #include "cris/core/signal/cr_signal.h"
 
-#include "gtest/gtest.h"
+#include <gflags/gflags.h>
+
+#include <gtest/gtest.h>
 
 #include <cstdio>
 
 int main(int argc, char** argv) {
     std::printf("Running main() from %s\n", __FILE__);
+    gflags::ParseCommandLineFlags(&argc, &argv, true);
     ::testing::InitGoogleTest(&argc, argv);
     cris::core::InstallSignalHandler();
     return RUN_ALL_TESTS();
