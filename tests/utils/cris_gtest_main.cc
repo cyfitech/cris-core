e926efc888cf1db4686077e18d9c67d455f8aa10
zhouxindong <34195701+zhouxindong@users.noreply.github.com>
2023-11-21 09:16:48 +0800

    Enable pass gflags arguments from command line in gtest (#238)

--- tests/utils/cris_gtest_main.cc
+++ tests/utils/cris_gtest_main.cc
@@ -1,5 +1,7 @@
 #include "cris/core/signal/cr_signal.h"
 
+#include "gflags/gflags.h"
+
 #include "gtest/gtest.h"
 
 #include <cstdio>
@@ -7,6 +9,7 @@
 int main(int argc, char** argv) {
     std::printf("Running main() from %s\n", __FILE__);
     ::testing::InitGoogleTest(&argc, argv);
+    gflags::ParseCommandLineFlags(&argc, &argv, false);
     cris::core::InstallSignalHandler();
     return RUN_ALL_TESTS();
 }
